503b1d11c2b73a45d30ac4e44ac20fc0c6aa113e
Maurice Carron <mcarron@gmail.com>
2019-03-01 19:02:27 -0800

    Tweet example.

--- src/main/scala/com/mritr/akka/streams/Tweet.scala
+++ src/main/scala/com/mritr/akka/streams/Tweet.scala
@@ -2,8 +2,10 @@ package com.mritr.akka.streams
 
 import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.{ActorMaterializer, ClosedShape, IOResult}
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, RunnableGraph, Sink, Source}
+
+import scala.concurrent.Future
 
 final case class Author(handle: String)
 
@@ -24,8 +26,8 @@ object TweetMain extends App {
   implicit val materializer = ActorMaterializer()
 
   val tweets: Source[Tweet, NotUsed] = Source(List(
-    Tweet(Author("mritr"), System.currentTimeMillis(), "Hey #akka"),
-    Tweet(Author("jsUser"), System.currentTimeMillis(), "another tweet")
+    Tweet(Author("mritr"), System.currentTimeMillis(), "Hey #akka #hashtag #woot"),
+    Tweet(Author("jsUser"), System.currentTimeMillis(), "another tweet #ok")
   ))
 
   val authors: Source[Author, NotUsed] = tweets
@@ -34,4 +36,25 @@ object TweetMain extends App {
 
   //authors.runWith(Sink.foreach(println))
   authors.runForeach(println)
+
+  // mapConcat is similar to flatMap
+  val hashtags: Source[Hashtag, NotUsed] = tweets.mapConcat(_.hashtags.toList)
+
+  hashtags.runForeach(println)
+
+  // Graphs allow for more complex flows
+  val writeAuthors: Sink[String, Future[IOResult]] = QuickStart.lineSink("authors.txt")
+  val writeHashtags: Sink[String, Future[IOResult]] = QuickStart.lineSink("hashtags.txt")
+
+  val g = RunnableGraph.fromGraph(GraphDSL.create() { implicit b =>
+    import GraphDSL.Implicits._
+
+    val bcast = b.add(Broadcast[Tweet](2))
+    tweets ~> bcast.in
+    bcast.out(0) ~> Flow[Tweet].map(_.author.toString) ~> writeAuthors
+    bcast.out(1) ~> Flow[Tweet].mapConcat(_.hashtags.toList).map(_.toString) ~> writeHashtags
+    ClosedShape
+  })
+  // This writes out the 2 files.
+  g.run()
 }
