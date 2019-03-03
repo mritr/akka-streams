b8a79df83a5d67e4d61656750a780968af9c9bac
Maurice Carron <mcarron@gmail.com>
2019-03-02 21:15:43 -0800

    Playing with graphs

--- src/main/scala/com/mritr/akka/streams/Graphs.scala
+++ src/main/scala/com/mritr/akka/streams/Graphs.scala
@@ -5,6 +5,7 @@ import akka.actor.ActorSystem
 import akka.stream.{ActorMaterializer, ClosedShape}
 import akka.stream.scaladsl._
 
+// https://doc.akka.io/docs/akka/2.5/stream/stream-graphs.html
 object Graphs extends App {
 
   implicit val system = ActorSystem("WorkingWithFlows")
@@ -32,7 +33,43 @@ object Graphs extends App {
 
     ClosedShape
   })
+  //val task = g.run()
 
-  val task = g.run()
+  // 2nd example
+  val topHeadSink = Sink.head[Int]
+  val bottomHeadSink = Sink.head[Int]
+  val sharedDoubler = Flow[Int].map(_ * 2)
+  val printer = Flow[Int].map(x => {
+    println(x)
+    x
+  })
+
+  val task2 = RunnableGraph.fromGraph(GraphDSL.create(topHeadSink, bottomHeadSink)((_, _)) { implicit builder =>
+    (topHS, bottomHS) =>
+      import GraphDSL.Implicits._
+      val broadcast = builder.add(Broadcast[Int](2))
+      Source.single(1) ~> broadcast.in
+
+      broadcast ~> sharedDoubler ~> printer ~> topHS.in
+      broadcast ~> sharedDoubler ~> printer ~> bottomHS.in
+      ClosedShape
+  })
+
+  //task2.run()
+
+  // Similar to what we're trying to do, multiple sources tied to a specific sink passing through a single flow.
+  val scratchTask =  RunnableGraph.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
+    import GraphDSL.Implicits._
+    val source1 = Source(1 to 10)
+    val source2 = Source(11 to 20)
+    val sink1 = Sink.ignore
+    val sink2 = Sink.ignore
+
+    source1 ~> sharedDoubler ~> printer ~> sink1
+    source2 ~> sharedDoubler ~> printer ~> sink2
+    ClosedShape
+  })
+
+  scratchTask.run()
 
 }
