c4b4a956f5b086cd5227448035e69fc635629020
Maurice Carron <mcarron@gmail.com>
2019-03-03 12:05:08 -0800

    custom shape, need to review

--- src/main/scala/com/mritr/akka/streams/Graphs.scala
+++ src/main/scala/com/mritr/akka/streams/Graphs.scala
@@ -2,7 +2,8 @@ package com.mritr.akka.streams
 
 import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.stream.{ActorMaterializer, ClosedShape, SourceShape, UniformFanInShape}
+import akka.stream.FanInShape.{Init, Name}
+import akka.stream._
 import akka.stream.scaladsl._
 
 import scala.collection.immutable
@@ -126,7 +127,7 @@ object Graphs extends App {
   max.onComplete(x => println(x))
 
   // constructing sources, sinks and flows from partial graphs.
-  val pairs = Source.fromGraph(GraphDSL.create() { implicit b =>
+  val pairs: Source[(Int, Int), NotUsed] = Source.fromGraph(GraphDSL.create() { implicit b =>
     import GraphDSL.Implicits._
 
     val zip = b.add(Zip[Int, Int]())
@@ -144,4 +145,108 @@ object Graphs extends App {
 
   // Expect this to be (1,2)
   firstPair.onComplete(x => println(x))
+
+  val pairUpWithToString =
+    Flow.fromGraph(GraphDSL.create() { implicit b =>
+      import GraphDSL.Implicits._
+
+      // prepare graph elements
+      val broadcast = b.add(Broadcast[Int](2))
+      val zip = b.add(Zip[Int, String]())
+
+      // connect the graph
+      broadcast.out(0).map(identity) ~> zip.in0
+      broadcast.out(1).map(_.toString) ~> zip.in1
+
+      // expose ports
+      FlowShape(broadcast.in, zip.out)
+    })
+
+  val f = pairUpWithToString.runWith(Source(List(1)), Sink.head)
+
+  f._2.onComplete(x => println(x))
+
+  // Combining Sources and Sinks with simplified API
+
+  // fanIn example
+  val sourceOne = Source(List(1))
+  val sourceTwo = Source(List(2))
+  val merged = Source.combine(sourceOne, sourceTwo)(Merge(_))
+
+  val mergedResult: Future[Int] = merged.runWith(Sink.fold(0)(_ + _))
+  // Expected result to be 3.
+  mergedResult.onComplete(x => println(x))
+
+  // Building reusable graph components
+  case class PriorityWorkerPoolShape[In, Out](
+                                             jobsIn: Inlet[In],
+                                             priorityJobsIn: Inlet[In],
+                                             resultsOut: Outlet[Out]
+                                             ) extends Shape {
+    override val inlets: immutable.Seq[Inlet[_]] =
+      jobsIn :: priorityJobsIn :: Nil
+    override val outlets: immutable.Seq[Outlet[_]] =
+      resultsOut :: Nil
+
+    override def deepCopy() = PriorityWorkerPoolShape(
+      jobsIn.carbonCopy(),
+      priorityJobsIn.carbonCopy(),
+      resultsOut.carbonCopy()
+    )
+  }
+
+  class PriorityWorkerPoolShape2[In, Out](_init: Init[Out] = Name("PriorityWorkerPool"))
+    extends FanInShape[Out](_init) {
+    protected override def construct(i: Init[Out]) = new PriorityWorkerPoolShape2(i)
+
+    val jobsIn = newInlet[In]("jobsIn")
+    val priorityJobsIn = newInlet[In]("priorityJobsIn")
+    // Outlet[Out] with name "out" is automatically created
+  }
+
+  object PriorityWorkerPool {
+    def apply[In, Out](
+                      worker: Flow[In, Out, Any],
+                      workerCount: Int
+                      ): Graph[PriorityWorkerPoolShape[In, Out], NotUsed] = {
+      GraphDSL.create() { implicit b =>
+        import GraphDSL.Implicits._
+
+        val priorityMerge = b.add(MergePreferred[In](1))
+        val balance = b.add(Balance[In](workerCount))
+        val resultsMerge = b.add(Merge[Out](workerCount))
+
+        priorityMerge ~> balance
+
+        for (i <- 0 until workerCount)
+          balance.out(i) ~> worker ~> resultsMerge.in(i)
+
+        PriorityWorkerPoolShape(
+          jobsIn = priorityMerge.in(0),
+          priorityJobsIn = priorityMerge.preferred,
+          resultsOut = resultsMerge.out
+        )
+
+      }
+    }
+  }
+
+  val worker1 = Flow[String].map("step 1 " + _)
+  val worker2 = Flow[String].map("step 2 " + _)
+
+  RunnableGraph.fromGraph(GraphDSL.create() { implicit b =>
+    import GraphDSL.Implicits._
+
+    val priorityPool1 = b.add(PriorityWorkerPool(worker1, 4))
+    val priorityPool2 = b.add(PriorityWorkerPool(worker2, 2))
+
+    Source(1 to 100).map("job: " + _) ~> priorityPool1.jobsIn
+    Source(1 to 100).map("priority job: " + _) ~> priorityPool1.priorityJobsIn
+
+    priorityPool1.resultsOut ~> priorityPool2.jobsIn
+    Source(1 to 100).map("one-step, priority " + _) ~> priorityPool2.priorityJobsIn
+
+    priorityPool2.resultsOut ~> Sink.foreach(println)
+    ClosedShape
+  }).run()
 }
